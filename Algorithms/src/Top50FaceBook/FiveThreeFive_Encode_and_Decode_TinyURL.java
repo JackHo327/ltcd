29ae6a05b337fb5ce6565ad39568445532e6524b
Renzhi He <renzhi_he@outlook.com>
2017-10-15 17:59:14 -0400

    10/15/2017

---
+++ Algorithms/src/Top50FaceBook/FiveThreeFive_Encode_and_Decode_TinyURL.java
@@ -0,0 +1,29 @@
+package Top50FaceBook;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Your Codec object will be instantiated and called as such:
+//Codec codec = new Codec();
+//codec.decode(codec.encode(url));
+public class FiveThreeFive_Encode_and_Decode_TinyURL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	Map<Integer, String> mp = new HashMap<Integer, String>();
+
+	// Encodes a URL to a shortened URL.
+	public String encode(String longUrl) {
+		mp.put(longUrl.hashCode(), longUrl);
+		return ("http://tinyurl.com/" + longUrl.hashCode());
+	}
+
+	// Decodes a shortened URL to its original URL.
+	public String decode(String shortUrl) {
+		return (mp.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", ""))));
+	}
+
+}
---
+++ Algorithms/src/Top50FaceBook/NineOne_Decode_Ways.java
@@ -0,0 +1,28 @@
+package Top50FaceBook;
+
+public class NineOne_Decode_Ways {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int numDecodings(String s) {
+		if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+			return 0;
+		} else {
+			int n1 = 1, n2 = 1, n3 = 0;
+			for (int i = 2; i <= s.length(); i++) {
+				n3 = 0;
+				if (s.charAt(i - 1) != '0')
+					n3 = n2;
+				int num = Integer.parseInt(s.substring(i - 2, i));
+				if (num >= 10 && num <= 26)
+					n3 += n1;
+				n1 = n2;
+				n2 = n3;
+			}
+			return n2;
+		}
+	}
+}
