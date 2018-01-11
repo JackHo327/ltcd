49d54097d05b78c64ab9abdaf865660ef33dc6d0
Renzhi He <renzhi_he@outlook.com>
2018-01-11 03:11:51 -0500

    changes

--- Algorithms/src/Algos/FourFiveOne_Sort_Characters_By_Frequency.java
+++ Algorithms/src/Algos/FourFiveOne_Sort_Characters_By_Frequency.java
@@ -1,85 +1,34 @@
 package Algos;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FourFiveOne_Sort_Characters_By_Frequency {
-
     public String frequencySort(String s) {
-
-        if (s == null || s.length() == 0) {
-            return "";
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-
-        if (s.length() == 1) {
-            return s;
+        List<Character>[] bucket = new List[s.length() + 1];
+        for (char key : map.keySet()) {
+            int frequency = map.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
         }
-
-        StringBuilder sb = new StringBuilder("");
-
-        // 1. turn String into char[]
-        char[] c = s.toCharArray();
-
-        // 2. sort the char[]
-        Arrays.sort(c);
-
-        // 3. turn sorted char[] back to String
-        String newS = new String(c);
-
-        // 4. do "word" count with HashMap <String, Integer>
-        Map<String, Integer> mp = new HashMap<>();
-        int slow = 0;
-        int fast = 1;
-        for (; fast < s.length(); fast++) {
-            if (c[fast] != c[slow]) {
-                int diff = fast - slow;
-                sb.append(newS.substring(slow, fast));
-                slow = fast;
-                mp.put(sb.toString(), diff);
-                sb = new StringBuilder("");
+        StringBuilder sb = new StringBuilder();
+        for (int pos = bucket.length - 1; pos >= 0; pos--) {
+            if (bucket[pos] != null) {
+                for (char num : bucket[pos]) {
+                    for (int i = 0; i < map.get(num); i++) {
+                        sb.append(num);
+                    }
+                }
             }
         }
-
-        int diff = fast - slow;
-        sb = new StringBuilder("");
-        while (slow < fast) {
-            sb.append(c[slow]);
-            slow++;
-        }
-        mp.put(sb.toString(), diff);
-
-        // 5. sort the HashMap by value descendingly
-        mp = sortByVal(mp);
-
-        // 6. concat keys
-        sb = new StringBuilder("");
-        for (String cs : mp.keySet()) {
-            sb.append(cs);
-        }
-
         return sb.toString();
-    }
-
-    // 5. sort the HashMap by value descendingly
-    private Map<String, Integer> sortByVal(Map<String, Integer> mp) {
-
-        List<Map.Entry<String, Integer>> list = new LinkedList<>(mp.entrySet());
-
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return o2.getValue().compareTo(o1.getValue());
-            }
-        });
-
-
-        // lambda is slower than the anonymous class
-        // Collections.sort(list, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
-
-        Map<String, Integer> newMp = new LinkedHashMap<>();
-        for (Map.Entry<String, Integer> entry : list) {
-            newMp.put(entry.getKey(), entry.getValue());
-        }
-
-        return newMp;
-
     }
 }
--- Algorithms/src/Algos/FourFourTwo_Find_All_Duplicates_in_an_Array.java
+++ Algorithms/src/Algos/FourFourTwo_Find_All_Duplicates_in_an_Array.java
@@ -5,27 +5,33 @@ import java.util.List;
 
 public class FourFourTwo_Find_All_Duplicates_in_an_Array {
 
-    public List<Integer> findDuplicates(int[] nums) {
+    public static void main(String[] args) {
+        findDuplicates(new int[]{4, 3, 2, 7, 4, 3, 2, 7});
+    }
+
+    public static List<Integer> findDuplicates(int[] nums) {
 
         List<Integer> retList = new ArrayList<Integer>();
 
-        if(nums == null|| nums.length == 0){
+        if (nums == null || nums.length == 0) {
             return retList;
         }
 
-        for(int i = 0; i < nums.length; i ++){
-            while(nums[i] != i+1 && nums[nums[i]-1] != nums[i]){
-
+//        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i + 1 && nums[nums[i] - 1] != nums[i]) {
+//                System.out.println(k++);
                 int tmp = nums[i];
-                nums[i] = nums[tmp-1];
-                nums[tmp-1] = tmp;
+                nums[i] = nums[tmp - 1];
+                nums[tmp - 1] = tmp;
 
             }
+//            System.out.println(Arrays.toString(nums));
         }
 
-        for(int i = 0; i < nums.length; i ++){
+        for (int i = 0; i < nums.length; i++) {
 
-            if(nums[i] != i+1){
+            if (nums[i] != i + 1) {
                 retList.add(nums[i]);
             }
 
