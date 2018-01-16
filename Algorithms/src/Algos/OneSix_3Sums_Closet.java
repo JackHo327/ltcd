09c3c4f56309a13596f649150ddd0230171499d6
Renzhi He <renzhi_he@outlook.com>
2018-01-16 03:23:06 -0500

    16-update

--- Algorithms/src/Algos/OneSix_3Sums_Closet.java
+++ Algorithms/src/Algos/OneSix_3Sums_Closet.java
@@ -3,36 +3,38 @@ package Algos;
 import java.util.Arrays;
 
 public class OneSix_3Sums_Closet {
-
-
+    
     public int threeSumClosest(int[] nums, int target) {
 
+        // sort the array - then could escape duplicated ones.
         Arrays.sort(nums);
 
-//        int diff = Integer.MAX_VALUE;
+        // create a tmp returned value -- and this is the MIN sum in the nums.
         int ret = nums[0] + nums[1] + nums[2];
 
         for (int i = 0; i + 2 < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {// skip same result
+            // skip same result
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             int j = i + 1, k = nums.length - 1;
             while (j < k) {
                 int sum = nums[j] + nums[k] + nums[i];
+                // if the sum is the target, then return target.
                 if (sum == target) {
                     return target;
                 } else {
-//                    if (diff > Math.abs(sum - target)) {
-//                        ret = sum;
-//                        diff = Math.abs(sum - target);
-//                    }
+                    // if the sum is not the target,
+                    // then check which one is closer to target
+                    // between ret and the current sum.
                     if (Math.abs(ret - target) > Math.abs(sum - target)) {
                         ret = sum;
-//                        diff = Math.abs(sum - target);
                     }
+                    // if sum is latger than target, then shrink the right bound
                     if (sum > target) {
                         k--;
                     } else {
+                        // else then enlarge the left bound.
                         j++;
                     }
                 }
